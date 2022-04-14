70393e1ed632e7433ab6f50a9223e8f28f4b30c9
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-04-14 17:41:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-
 document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('.random').addEventListener('click', getRandom)
 
 function getFetch(){
   const choice = document.querySelector('input').value
@@ -11,11 +11,41 @@ function getFetch(){
         console.log(data)
         if(data.media_type === 'image'){
           document.querySelector('.photoDay').src = data.hdurl
+          document.querySelector('.photoDay').classList.remove('hidden')
+          document.querySelector('iframe').classList.add('hidden')
         }else if(data.media_type === 'video'){
           document.querySelector('iframe').src = data.url
+          document.querySelector('.photoDay').classList.remove('hidden')
+          document.querySelector('iframe').classList.add('hidden')
         }
         document.querySelector('.photoDay').src = data.hdurl
         document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
+
+function getRandom(){
+  // const choice = document.querySelector('input').value
+  // console.log(choice);
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=dAlLs6Ve3SJEbMfzJxr32dni9cvKFYZClqBS5SbN&count=1'
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if(data[0].media_type=== 'image'){
+        document.querySelector('.photoDay').src = data[0].hdurl
+        document.querySelector('.photoDay').classList.remove('hidden')
+        document.querySelector('iframe').classList.add('hidden')
+        } else if(data[0].media_type === 'video'){
+          document.querySelector('iframe').src = data[0].url
+          document.querySelector('iframe').classList.remove('hidden')
+          document.querySelector('.photoDay').classList.add('hidden')
+        }
+        document.querySelector('h3').innerText = data[0].explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
